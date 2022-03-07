c396142edbc0a37c0334312f8abe1333b4b1628a
jae1choi <jw.choi@mxncommerce.com>
2022-03-07 12:27:37 +0900

    api default params

--- src/apis/data.ts
+++ src/apis/data.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
 import { QuizResponse } from "../types/data";
 
-export const getQuiz = async () =>
+export const getQuiz = async ({
+  type = "multiple",
+  amount = 10,
+}: {
+  type?: string;
+  amount?: number;
+}) =>
   await axios.get<QuizResponse>("https://opentdb.com/api.php", {
     params: {
-      type: "multiple",
-      amount: 10,
+      type,
+      amount,
     },
   });
